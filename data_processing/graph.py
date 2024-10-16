9542d5cd5227d5b4e1015afaaa44fc6728ea8834
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2024-10-15 21:38:39 -0300

    finished create crimeGraph

--- data_processing/graph.py
+++ data_processing/graph.py
@@ -31,9 +31,9 @@ def createGeoDicts(gdf, indexColumn):
         indexToGeo[gdf[indexColumn][i]] = gdf['geometry'][i]
     return geoToIndex, indexToGeo
 
-def createEdgesDf(gdf, indexColumn, k_neighbors=3):
+def createEdgesDf(gdf, indexColumn, kNeighbors=3):
     #array to temp store edges
-    edgesArray = np.zeros((len(gdf)*k_neighbors, 3), dtype=np.int64)
+    edgesArray = np.zeros((len(gdf)*kNeighbors, 3), dtype=np.int64)
     #creates dictionary that maps point to geometry
     geoToIndex, indexToGeo = createGeoDicts(gdf, indexColumn)
     #transform geometry so that there are no two points with the same coordinates
@@ -45,7 +45,7 @@ def createEdgesDf(gdf, indexColumn, k_neighbors=3):
         currentGeo = row['geometry']
         currentPoint = (currentGeo.x, currentGeo.y)
         #finds k nearest points
-        dist, indexes = tree.query(currentPoint, k=k_neighbors+1)
+        dist, indexes = tree.query(currentPoint, k=kNeighbors+1)
         #first point is the point which we used to query
         for j, index in enumerate(indexes):
             if j == 0:
@@ -55,9 +55,106 @@ def createEdgesDf(gdf, indexColumn, k_neighbors=3):
             point = shapely.Point(point)
             #gets index of gdf which the point represents
             actual_index = geoToIndex[point]
-            edgesArray[i*k_neighbors+j-1] = [row[indexColumn], actual_index, dist[j]]
+            edgesArray[i*kNeighbors+j-1] = [row[indexColumn], actual_index, dist[j]]
     edgesDf = pd.DataFrame(edgesArray, columns=['u', 'v', 'dist'])
     return edgesDf
+
+#receives crimedf joined with city graph nodes
+def createNodesDf(mergedGdf):
+    dfSize = len(mergedGdf['index_right'].unique())
+    columns=["node_id", "latitude", "longitude", "amount", "dawn", "morning", "afternoon", "night"]
+    #index for first time column
+    TIME = columns.index("dawn")
+    #index for amount colum
+    AMOUNT= columns.index("amount")
+    GEO = columns.index("latitude")
+    #constructed using stolen vehicles dataset from 2017 - 2024
+    local_subtype = [
+    'acougue/frigorífico-câmara frigorífica', 'via pública-transeunte', 'area de descanso-interior de veiculo particular',
+    'mercado', 'oficina', 'parque/bosque/horto/reserva',
+    'auto-peças', 'praça de pedágio-cabine/posto', 'bar/botequim',
+    'delegacia/distrito policial', 'padaria/confeitaria', 'armazém/empório',
+    'lanchonete/pastelaria/pizzaria-outros', 'via pública-ciclofaixa', 'interior veículo de carga',
+    'metroviário e ferroviário metropolitano', 'feira-livre-outros', 'praça de pedágio-outros',
+    'açougue/frigorífico', 'rodoviário', 'obra/construção',
+    'área de descanso', 'posto de auxílio', 'lote de terreno',
+    'tunel/viaduto/ponte-transeunte', 'transportadora', 'atacadista',
+    'conveniência', 'estacionamento', 'area de descanso-transeunte',
+    'praça', 'acougue/frigorífico-outros', 'posto de auxílio-interior de veiculo de carga',
+    'rodoviário-outros', 'fabrica/indústria', 'fábrica/indústria',
+    'estacionamento particular', 'balança-outros', 'loja de material de construção',
+    'mecânica/borracharia', 'restaurante-outros', 'sala de reuniões/convenções',
+    'interior de transporte coletivo', 'tunel/viaduto/ponte-interior de veiculo de carga', 'estacionamento público',
+    'via pública', 'lojas', 'posto de gasolina',
+    'via pública-interior de veiculo de carga', 'area de descanso-interior de veiculo de carga', 'interior de veículo particular',
+    'praça de pedágio', 'semáforo-outros', 'farmácia/drogaria',
+    'area de descanso-outros', 'terreno baldio', 'restaurante-estacionamento',
+    'ciclofaixa', 'semáforo-interior de veiculo particular', 'posto policial-outros',
+    'rodoviário-estacioanamento', 'acostamento-interior de veiculo de carga', 'hospital-outros',
+    'distribuidora', 'praça de pedágio-estacionamento', 'veículo em movimento',
+    'posto de auxílio-interior de veiculo particular', 'praça-outros', 'construção abandonada',
+    'posto de fiscalização-outros', 'interior de veículo de carga',
+    'outros', 'hospital-almoxarifado', 'balança',
+    'semáforo', 'agência-outros', 'tunel/viaduto/ponte-outros',
+    'restaurante', 'depósito', 'outros-estacionamento',
+    'posto de fiscalização', 'praça-interior de veículo de carga', 'lanchonete/pastelaria/pizzaria-estacionamento',
+    'loteamento', 'acostamento-outros', 'posto de auxílio-estacionamento',
+    'bar/botequim-outros', 'rodoviário-garagem', 'delegacia',
+    'hospital', 'praça de pedágio-interior de veiculo de carga', 'túnel/viaduto/ponte',
+    'via pública-interior de veiculo particular', 'posto de auxílio-cabine/posto/escritório', 'transeunte',
+    'desmanche', 'metalúrgica-outros', 'posto de auxílio-outros',
+    'aeroportuário', 'semáforo-interior de veiculo de carga', 'acostamento-transeunte',
+    'acostamento', 'outros-banheiro', 'portuário-outros',
+    'área comum'
+    ]
+    SUBTYPE = len(columns)
+    subTypeIndex = {}
+    #associates subtype to index in order to encode
+    for i, v in enumerate(local_subtype):
+        subTypeIndex[v] = i
+    rowsNpArray = np.zeros((dfSize, len(columns)+len(local_subtype)))
+    #maps node id to idnex in df
+    nodeIdToIndex = {}
+    #to keep track of nodes id
+    currentIndex = 0
+    #loops through rows building the df
+    for i, row in mergedGdf.iterrows():
+        #checks if nodeId already in rowsArray
+        indexInDf = nodeIdToIndex.get(row['index_right'])
+        #must add node entry to array
+        if indexInDf == None:
+            nodeIdToIndex[row['index_right']] = currentIndex
+            indexInDf = currentIndex
+            rowsNpArray[indexInDf][0] = row['index_right']
+            rowsNpArray[indexInDf][GEO] = row['LATITUDE']
+            rowsNpArray[indexInDf][GEO+1] = row['LONGITUDE']
+            currentIndex += 1
+        #adds crime ocorrence to graph
+        rowsNpArray[indexInDf][AMOUNT]+=1
+        #parses time
+        time = row['HORA_OCORRENCIA']
+        if not pd.isnull(time):
+            time = int(time[0:2])
+            #so we can get the index
+            timeShift = time//6
+        rowsNpArray[indexInDf][TIME + timeShift] += 1
+        #parses subtype
+        subType = row['DESCR_SUBTIPOLOCAL']
+        if not pd.isnull(subType) and subTypeIndex.get(subType.lower()) is not None:
+            encodedIndex = subTypeIndex[subType.lower()]
+            rowsNpArray[indexInDf][SUBTYPE + encodedIndex] += 1
+    #update columns to have descr_subtipolocal
+    columns = columns + local_subtype
+    nodesDf = pd.DataFrame(rowsNpArray, columns=columns)
+    return nodesDf
+
+def createCrimeGraphDf(crimeOcorrencesGdf, cityGraphNodes, kNeighbors=3):
+    #projects ocorrences into citygraph
+    merge = joinCrimeWithCity(crimeOcorrencesGdf, cityGraphNodes)
+    edgesDf = createEdgesDf(merge, "index_right", kNeighbors)
+    nodesDf = createNodesDf(merge)
+    return nodesDf, edgesDf
+
 
 #converts graph to gpd and saves it
 def saveGraphToDisk(graph, graphName):
